c0ced049c00c8d7beb8f847e6eec1c03d06c791d
peixy <pei.xy0@outlook.com>
2023-01-24 14:33:34 +0800

    add websocket support part2

--- src/main/app.hpp
+++ src/main/app.hpp
@@ -1,7 +1,14 @@
 #pragma once
+#include <optional>
 #include "network.hpp"
 
 namespace application {
+
+struct WebsocketFrame {
+  bool fin;
+  std::uint8_t opcode;
+  std::string payload;
+};
 
 class WebsocketLayer {
 public:
@@ -9,7 +16,17 @@ public:
   void Process(network::HttpRequest&&);
 
 private:
+  std::optional<WebsocketFrame> ExtractFrame(std::string&);
+  void ProcessMessage(std::string&);
+
   network::HttpSender& sender;
+  std::string received;
+  std::string message;
+  static constexpr std::uint8_t headerLen = 2;
+  static constexpr std::uint8_t maskLen = 4;
+  static constexpr std::uint8_t ext1Len = 2;
+  static constexpr std::uint8_t ext2Len = 8;
+  static constexpr std::uint8_t opClose = 8;
 };
 
 struct AppOptions {
